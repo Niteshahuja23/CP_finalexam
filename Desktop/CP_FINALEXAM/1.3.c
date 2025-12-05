386862e6a45740fd31721eef1597c3aacde26e4a
Niteshahuja23 <Ahujanitesh9a.roll37@gmail.com>
2025-12-06 00:28:32 +0530

    1.3

---
+++ Desktop/CP_FINALEXAM/1.3.c
@@ -0,0 +1,24 @@
+//Write a program to demonstrate Bitwise operators.
+#include <stdio.h>
+void main()
+{
+    int a = 5,b = 10,ans;
+    // Bitwise AND operator
+    ans = a & b;
+    printf("a & b: %d\n",ans);
+    // Bitwise OR operator
+    ans = a|b;
+    printf("a|b: %d\n",ans);
+    // Bitwise XOR operator
+    ans  = a^b;
+    printf("a^b: %d\n",ans);
+    // Bitwise NOT operator
+    ans = ~a;
+    printf("~a: %d\n",ans);
+    // Left shift operator
+    ans = a<<b;
+    printf("a<<b: %d\n",ans);
+    // Right shift operator
+    ans = a>>b;
+    printf("a>>b: %d\n",ans);
+}
