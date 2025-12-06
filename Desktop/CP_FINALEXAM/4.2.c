3423d7b1defb21d3ece23effaced0673318fdfd9
Niteshahuja23 <Ahujanitesh9a.roll37@gmail.com>
2025-12-06 11:08:39 +0530

    4.2

---
+++ Desktop/CP_FINALEXAM/4.2.c
@@ -0,0 +1,16 @@
+//Write a program to find the sum of digits of a number.
+#include <stdio.h>
+void main()
+{
+    int n,sum,digit;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    sum = 0;
+    while(n != 0)
+    {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+    printf("The sum of digits of the given number is: %d",sum);
+}
