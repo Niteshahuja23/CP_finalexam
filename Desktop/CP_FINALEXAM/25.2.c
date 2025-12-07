7a0c24e251821139b769c6500f591f0797434d0d
Niteshahuja23 <Ahujanitesh9a.roll37@gmail.com>
2025-12-08 01:09:15 +0530

    26

--- Desktop/CP_FINALEXAM/25.2.c
+++ Desktop/CP_FINALEXAM/25.2.c
@@ -1,1 +1,18 @@
 //Write a program to perform addition using call by reference.
+#include<stdio.h>
+void main()
+{
+    int a=5,b=10,s;
+    int sum(int *,int *);
+    int*p1,*p2;
+    p1 = &a;
+    p2 = &b;
+    s = sum(p1,p2);
+    printf("%d",s);
+}
+int sum(int *p1,int *p2)
+{
+    int s;
+    s = *p1 + *p2;
+    return s;
+}
